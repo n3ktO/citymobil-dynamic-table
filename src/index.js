4019645da77c014bc5139b58f8bb90deec428f3e
n3ktO <ruslannekto@gmail.com>
2021-07-19 23:07:58 +0300

    Fix createGlobalStyle font warning.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { createGlobalStyle } from 'styled-components';
 import App from './App';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap');
-
 html {
   font-family: 'Roboto', sans-serif;
 }
